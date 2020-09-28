d3e7dfd98efd6e573f320da98c5f605171d96172
markwhat <whatcott1@gmail.com>
2020-09-28 13:27:09 -0500

    Completed the addition of game values by manager This version is working perfectly

--- schedule_check.py
+++ schedule_check.py
@@ -2,6 +2,7 @@ import collections
 import functools
 import operator
 from pprint import pprint
+import logging
 
 import arrow
 from prettytable import PrettyTable
@@ -10,65 +11,77 @@ from yfpy import Data
 from yfpy.query import YahooFantasySportsQuery
 
 
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 # logging.debug('Script has started.')
 
 
 def get_roster_players(team_id_list):
     global game_week
+    global managers
     pos_values = {
-        'QB': 1.2,
+        'QB': 1.0,
         'RB': 1.0,
         'WR': 1.0,
         'TE': 1.0,
         'W/R/T': 1.0,
         'DEF': 1.0,
         'K': 1.0,
-        'D': 0.8,
+        'D': 0.5,
     }
 
-    player_list = list()
+    all_manager_players = list()
     for team_id in team_id_list:
         # Load team info; returns a Team object
         manager_info = yf.get_team_info(team_id=team_id)
 
         # Get manager name
         manager_name = get_manager_name(manager_info)
+        managers.append(manager_name)
 
         # Set current game_week
         update_current_week(manager_info)
 
         # Load roster info; returns a list of Player objects
         players = yf.get_team_roster_player_info_by_week(team_id=team_id)
+        manager_player_dict = dict()
+        manager_player_dict['manager'] = manager_name
+        manager_player_list = list()
         for player in players:
             player = player['player']
             player_pos = player.primary_position
             roster_pos = player.selected_position_value  # Position in team's roster
             if roster_pos != 'BN':
-                if player.is_undroppable == '1':
-                    player_value = round(pos_values.get(player_pos) * 1.5, 1)
-                    is_undroppable = True
-                else:
-                    player_value = pos_values.get(player_pos)
-                    is_undroppable = False
+                # if player.is_undroppable == '1':
+                #     player_value = round(pos_values.get(player_pos) * 1.5, 1)
+                #     is_undroppable = True
+                # else:
+                #     player_value = pos_values.get(player_pos)
+                #     is_undroppable = False
+                player_value = pos_values.get(player_pos)
                 player_dict = {
                     'name': player.name.full,
                     'team_name': player.editorial_team_full_name,
                     'team_abbr': player.editorial_team_abbr,
                     'pos': player.primary_position,
                     'player_value': player_value,
-                    'is_undroppable': is_undroppable,
-                    'manager': manager_name
+                    # 'is_undroppable': is_undroppable
+                    # 'manager': manager_name
                 }
-                player_list.append(player_dict)
+                manager_player_list.append(player_dict)
+        manager_player_dict['players'] = manager_player_list
+        all_manager_players.append(manager_player_dict)
 
-    return player_list
+    return all_manager_players
 
 
 def get_manager_name(team_obj):
-    managers = team_obj.managers
-    manager = managers['manager']
-    return manager.nickname
+    all_managers = team_obj.managers
+    manager = all_managers['manager']
+    manager_name = manager.nickname
+    manager_name = manager_name.split(' ')[0]
+    if manager_name == '--':
+        manager_name = 'Deric\'s Dad'
+    return manager_name
 
 
 def update_current_week(team_obj):
@@ -85,10 +98,9 @@ def get_weekly_schedule(week):
     game_list = nfl.get_weekly_schedule(year=2020, nfl_season='REG', nfl_season_week=week).json()
     print(game_list)
 
-    if int(game_list['week']['title']) != int(game_week):
-        print('Error finding week')
-    else:
-        game_list = game_list['week']['games']
+    assert (int(game_list['week']['title']) == int(game_week)), "Week from SportsRadar doesn't match Yahoo"
+
+    game_list = game_list['week']['games']
 
     game_dict_list = list()
     for item in game_list:
@@ -107,61 +119,65 @@ def get_weekly_schedule(week):
         game_dict['teams'] = [away, home]
         game_dict['game_title'] = f'{away_abbr} vs. {home_abbr}'
         game_dict['broadcast'] = item['broadcast']['network']
-        game_dict['values'] = list()
-        game_dict['players'] = list()
         game_dict_list.append(game_dict)
 
     return game_dict_list
 
 
-def assign_players_to_games(week_games, player_list):
+def assign_players_to_games(week_games, manager_player_list):
     for week_game in week_games:
-        game_players = week_game.get('players', [])  # Returns list of players
-        player_count = 0
-        for player in player_list:
-            if player['team_name'] in week_game['teams']:
-                player_count += 1
-                player.update({'num': player_count})
-                game_players.append(player)
-
-            week_game['players'] = game_players
-    print(weekly_games)
+        for manager in manager_player_list:
+            manager_name = manager['manager']
+            players = manager['players']
+            game_player_list = list()
+            for player in players:
+                if player['team_name'] in week_game['teams']:
+                    game_player_list.append(player)
+            week_game[manager_name] = game_player_list
     return week_games
 
 
 def sum_player_values(week_games):
     for week_game in week_games:
-        week_game['values'] = sum(week_game['values'])
-        if week_game['values'] == 0:
-            week_game['values'] = float(0.0)
-
+        total_value = list()
+        for manager in managers:
+            if week_game[manager]:
+                player_list = week_game[manager]
+                manager_value = sum(x['player_value'] for x in player_list)
+                week_game[manager] = manager_value
+                total_value.append(manager_value)
+            else:
+                week_game[manager] = float(0.0)
+        week_game['Total'] = sum(total_value)
     return week_games
 
 
 def sort_game_list(week_games):
-    return sorted(week_games, key=lambda k: (k['game_time'], -k['values']))
+    return sorted(week_games, key=lambda k: (k['game_time'], -k['Total']))
 
 
 def create_print_table(game_list):
     x = PrettyTable()
-    x.field_names = ['Score', 'Game Time', 'Channel', 'Match']
-    x.align['Game Time'] = 'l'
-    x.align['Channel'] = 'c'
-    x.align['Match'] = 'l'
+    first_fields = ['Game Time', 'Match', 'Channel']
+    x.field_names = [*first_fields, *managers, 'Total']
+    # x.align['Game Time'] = 'l'
+    # x.align['Channel'] = 'c'
+    # x.align['Match'] = 'l'
 
-    for game in game_list:
-        score = game['values']
-        match = game['away_team'] + ' vs. ' + game['home_team']
-        game_time = arrow.get(game['game_time'])
+    for item in game_list:
+        match = item['away_abbr'] + ' vs. ' + item['home_abbr']
+        game_time = arrow.get(item['game_time'])
         game_time_human = game_time.to('US/Central').humanize(granularity=["hour"])
+        broadcast = item['broadcast']
         if game_time_human == "in 0 hours":
             game_time_human = "now"
-        broadcast = game['broadcast']
+        row = [game_time_human, match, broadcast]
+        for manager in managers:
+            row.append(item[manager])
+        row.append(item['Total'])
         if game_time > arrow.now():
-            x.add_row([score, game_time_human, broadcast, match])
-
-    # TODO: Split value per game by
-
+            x.add_row(row)
+        print(row)
     return x
 
 
@@ -172,9 +188,10 @@ if __name__ == "__main__":
     game_id = "399"
     game_code = "nfl"
     all_output_as_json = False
-    game_week = 2
+    game_week = 3
+    managers = list()
     team_IDs = [3, 4]  # '1', '9']  # 3 = Mark, 4 = Lauren
-
+    # team_IDs = range(1, 11)
     yf = YahooFantasySportsQuery(auth_dir=auth_dir,
                                  league_id=league_id,
                                  game_id=game_id,
@@ -182,22 +199,20 @@ if __name__ == "__main__":
                                  all_output_as_json=all_output_as_json)
 
     # Fetch and combine rosters by team using yfpy
-    players = get_roster_players(team_IDs)
-    print(players)
+    all_players = get_roster_players(team_IDs)
 
     # Fetch weekly NFL scheduled
     weekly_games = get_weekly_schedule(week=game_week)
 
     # Assign player position values, sum by team
-    weekly_games = assign_players_to_games(week_games=weekly_games, player_list=players)
-    print(weekly_games)
+    weekly_games = assign_players_to_games(week_games=weekly_games, manager_player_list=all_players)
 
-    #
-    # # Sort list of games by game_time ASC, values DESC
-    # weekly_games = sort_game_list(weekly_games)
-    #
-    # # Use PrettyTable to print table of game times
-    # table = create_print_table(weekly_games)
-    # print(table)
+    # Sum player values by Manager
+    weekly_games = sum_player_values(weekly_games)
 
-    # Tools to sum list of dictionaries
+    # Sort list of games by game_time ASC, values DESC
+    weekly_games = sort_game_list(weekly_games)
+
+    # Use PrettyTable to print table of game times
+    table = create_print_table(weekly_games)
+    print(table)
